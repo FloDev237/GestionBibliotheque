32ebca1fa6fe322febfc19bae90c7bce9c2dbbc0
DjenabouH <hamawadjenabou66@gmail.com>
2026-04-29 01:29:28 +0200

    par DJUIDJEwoupa

--- src/model/Livre.java
+++ src/model/Livre.java
@@ -1,5 +1,54 @@
 package model;
 
 public class Livre {
-    
+    private int id;
+    private String titre;
+    private String auteur;
+    private int quantite;
+    private String isbn;
+    public Livre(int id, String titre, String auteur, int quantite, String isbn) {
+        this.id = id;
+        this.titre = titre;
+        this.auteur = auteur;
+        this.quantite = quantite;
+        this.isbn = isbn;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getTitre(){
+        return titre;
+    }
+    public String getAuteur(){
+        return auteur;
+    }
+    public int getQuantite(){
+        return quantite;
+    }
+    public String getIsbn(){
+        return isbn;
+    }
+    public boolean isDisponible(){
+        return quantite > 0;
+    }
+    public void emprunter(){
+        if (isDisponible()) {
+            quantite--;
+        } else {
+            System.out.println("Livre non disponible pour emprunt.");
+        }
+    }
+    public void retourner(){
+        quantite++;
+    }
+    @Override
+    public String toString(){
+        return "Livre{" +
+                "id=" + id +
+                ", titre='" + titre + '\'' +
+                ", auteur='" + auteur + '\'' +
+                ", quantite=" + quantite +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
 }
--- src/model/Membre.java
+++ src/model/Membre.java
@@ -1,5 +1,66 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Membre {
+    private int idMembre;
+    private String nom;
+    private String email;
+    private String adresse;
+    private int tel;
+    private List<Livre> livresEmpruntes;
     
+    public Membre(int idMembre, string nom, String email, String adresse, int tel){
+        this.idMembre = idMembre;
+        this.nom = nom;
+        this.email = email;
+        this.adresse = adresse;
+        this.tel = tel;
+        this.livresEmpruntes = new Arraylist<>();
+    }
+    public int getIdMembre(){
+        return idMembre;
+    }
+    public String getNom(){
+        return nom;
+    }
+    public String getEmail(){
+        return email;
+    }
+
+    public String getAdresse(){
+        return adresse;
+    }
+    public int getTel(){
+        return tel;
+    }
+    public List<Livre> getLivresEmpruntes(){
+        return livresEmpruntes;
+    }
+    public void emprunterLivre(Livre livre){
+        if (livre.isDisponible()) {
+            livre.emprunter();
+            livresEmpruntes.add(livre);
+        } else {
+            System.out.println("Livre non disponible pour emprunt.");
+        }
+    }
+    public void retournerLivre(Livre livre){
+        if (livresEmpruntes.remove(livre)) {
+            livre.retourner();
+        } else {
+            System.out.println("Ce livre n'est pas emprunté par ce membre.");
+        }
+    }
+    @Override
+    public String toString(){
+        return "Membre{" +
+                "idMembre=" + idMembre +
+                ", nom='" + nom + '\'' +
+                ", email='" + email + '\'' +
+                ", adresse='" + adresse + '\'' +
+                ", tel=" + tel +
+                '}';
+    }
 }
